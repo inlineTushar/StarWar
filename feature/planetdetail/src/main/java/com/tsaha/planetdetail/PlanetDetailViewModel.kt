462cd5cef593b76e1bcbb9a41e68dc96548203e2
Tushar Saha <tsaha.cse@gmail.com>
2025-09-16 16:59:20 +0200

    Refactor getPlanetDetail to simplify state updates
    Replaces the use of channelFlow and onEach with direct state updates in getPlanetDetail. This simplifies the coroutine flow and improves readability by updating the UI state directly on success or failure.

--- feature/planetdetail/src/main/java/com/tsaha/planetdetail/PlanetDetailViewModel.kt
+++ feature/planetdetail/src/main/java/com/tsaha/planetdetail/PlanetDetailViewModel.kt
@@ -4,12 +4,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.tsaha.nucleus.data.repository.PlanetRepository
 import com.tsaha.nucleus.ui.PlanetDetailsUiState
-import com.tsaha.nucleus.ui.PlanetDetailsUiState.DetailsLoading
+import com.tsaha.nucleus.ui.PlanetDetailsUiState.*
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.channelFlow
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 
@@ -28,11 +26,8 @@ class PlanetDetailViewModel(
         )
 
     suspend fun getPlanetDetail(planetId: String) {
-        channelFlow {
-            planetRepository.getPlanet(planetId)
-                .onSuccess { planet -> send(PlanetDetailsUiState.DetailsSuccess(planet)) }
-                .onFailure { send(PlanetDetailsUiState.DetailsError(it.message)) }
-        }.onEach { uiStateMutable.value = it }
-            .stateIn(viewModelScope)
+        planetRepository.getPlanet(planetId)
+            .onSuccess { planet -> uiStateMutable.value = DetailsSuccess(planet) }
+            .onFailure { uiStateMutable.value = DetailsError(it.message) }
     }
 }
